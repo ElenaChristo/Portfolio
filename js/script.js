968b9309acb89cc5308f5a844fb0988847dcbfe7
Elena Christodoulou <elena.c@live.co.uk>
2018-07-17 16:39:48 +0100

    Added to js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
-    $("form").validate();
+    $('form').validate();
+
+   
+    $('#second-box').hide();
+    $('#first-box').hide();
+    $('#third-box').hide();
+
+    $('#box1').on('mouseenter', () => {
+        $('#first-box').show("slow");
+    }).on('mouseleave', () => {
+        $('#first-box').hide('slow');
+    });
+
+    $('#box2').on('mouseenter', () => {
+        $('#second-box').show('slow');
+    }).on('mouseleave', () => {
+        $('#second-box').hide('slow');
+    });
+
+    $('#box3').on('mouseenter', () => {
+        $('#third-box').show('slow');
+    }).on('mouseleave', () => {
+        $('#third-box').hide('slow');
+    });
+
+
+    
 })
